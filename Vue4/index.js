0f6a98210c76fb52a2bcd22a04ba3a6348348a0c
Lexie <sb.alexandria@gmail.com>
2018-09-24 15:48:36 +0530

    Computed Properties

--- Vue4/index.js
+++ Vue4/index.js
@@ -20,29 +20,48 @@ var app = new Vue({
 });
 
 var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        logName: function() {
-            console.log('My name is');
-        },
-        logAge: function() {
-            console.log('My age is');
-        }
+  el: "#app-2",
+  data: {
+    logName: function() {
+      console.log("My name is");
+    },
+    logAge: function() {
+      console.log("My age is");
     }
-})
+  }
+});
 
 var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        name: '',
-        age: ''
+  el: "#app-3",
+  data: {
+    name: "",
+    age: ""
+  },
+  methods: {
+    logName: function() {
+      console.log("My name is");
     },
-    methods: {
-        logName: function() {
-            console.log('My name is');
-        },
-        logAge: function() {
-            console.log('My age is');
-        }
+    logAge: function() {
+      console.log("My age is");
     }
-})
+  }
+});
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+    a: 0,
+    b: 0,
+    age: 80
+  },
+  computed: {
+    agePlusA: function(){
+        console.log('agePlusA');
+        return this.a + this.age;
+    },
+    agePlusB: function(){
+        console.log('agePlusB');
+        return this.b + this.age;
+    }
+  }
+});
