38ad4ae1122cf0a03055b36409490f0d0c0cd96c
Lexie <sb.alexandria@gmail.com>
2018-10-09 23:09:15 +0530

    Added events

--- Flower-game/app.js
+++ Flower-game/app.js
@@ -1,7 +1,34 @@
 var app = new Vue({
   el: "#app",
   data: {
-    flowers: 
+    mousemove: function(event) {
+      flower.style.position = 'absolute';
+      flower.style.zIndex = 100;
+      document.body.append(flower);
+      moveAt(event.pageX, event.pageY);
+
+      function moveAt(pageX, pageY){
+        flower.style.left= pageX - flower.offsetWidth / 10 + 'px';
+        flower.style.top= pageY - flower.offsetWidth / 10 + 'px';
+      }
+
+      function onMouseMove(event){
+        moveAt(event.pageX, event.pageY);
+      }
+
+      document.addEventListener('mousemove', onMouseMove);
+
+      this.flower.onmouseup = function(){
+        document.removeEventListener('mousemove', onMouseMove);
+        flower.onmouseup = null;
+      };
+
+      this.flower.ondragstart = function() {
+        return false;
+      };
+
+      
+    }
      
 }
 })
