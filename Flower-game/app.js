5bd011e41cc399e4147a6119cd0c53e63a372abb
Lexie <sb.alexandria@gmail.com>
2018-10-14 22:55:03 +0530

    working on score

--- Flower-game/app.js
+++ Flower-game/app.js
@@ -1,8 +1,13 @@
-Vue.component('flower-component', {
+Vue.component("flower-component", {
   data() {
-   return {
-     score: 0
-   }
+    return {
+      score: 0
+    };
+  },
+  methods: {
+    countScore: function() {
+      this.score += 10;
+    }
   },
   props: ["id", "image"],
   template: "#flower-component"
@@ -13,21 +18,20 @@ var app = new Vue({
   el: "#app",
   data() {
     return {
-    flowers: [
-      {
-        position: "flower1",
-        image: "./img/flower-1.png"
-      },
-      {
-        id: "flower2",
-        image: "./img/flower-2.png"
-      },
-      {
-        id: "flower3",
-        image: "./img/flower-3.png"
-      }
-    ]
-   
-  };
- }
+      flowers: [
+        {
+          position: "flower1",
+          image: "./img/flower-1.png"
+        },
+        {
+          id: "flower2",
+          image: "./img/flower-2.png"
+        },
+        {
+          id: "flower3",
+          image: "./img/flower-3.png"
+        }
+      ]
+    };
+  }
 });
